49b2f56716eadd562a98d01bd1d7bb5dcc771716
Abhinav <abhinavprajapait2001@gmail.com>
2024-01-27 16:10:52 +0530

    Add status led and Remvoe unused code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include "Commands.h"
 #include "BTS7960.h"
 #include "Cytron_MD30C.h"
+#include <FastLED.h>
+
+// Status Led init
+#define LED_PIN 17
+#define NUM_LEDS 2
+#define BRIGHTNESS 200
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
 
 #define _LOG_MOTOR_PWM_ 0
 #define _LOG_ENCODER_TICK_RPM_ 0
@@ -13,25 +23,6 @@
 // Specify motor Driver
 #define _MDDS30_
 // #define _Motor_cytron_md30_
-
-// PPR of motor
-#define PPR 1300
-// How frequently RPM should be calculated
-#define RPM_TIMER_INTERVAL_MS 20
-
-// #define ENCODER_2_IN1 26
-// #define ENCODER_2_IN2 22
-
-// #define ENCODER_3_IN1 21
-// #define ENCODER_3_IN2 20
-
-// #define ENCODER_1_IN1 28
-// #define ENCODER_1_IN2 27
-
-volatile int32_t counts = 0;
-volatile int32_t previous_encoder_ticks = 0;
-volatile float RPM = 0;
-
 struct BusData
 {
   int16_t motor_1;
@@ -42,41 +33,12 @@ struct BusData
 };
 struct BusData DriveData = {0, 0, 0, 0};
 
-#ifdef _BTS7960_
-
-BTS7890 motor_1(6, 7);
-BTS7890 motor_2(8, 9);
-BTS7890 motor_3(12, 14);
-
-#endif
-
 #ifdef _MDDS30_
-
 CytronMD motor_1(PWM_DIR, 8, 12);  // PWM 1 = Pin 3, DIR 1 = Pin 4.
 CytronMD motor_2(PWM_DIR, 11, 13); // PWM 2 = Pin 9, DIR 2 = Pin 10.
 CytronMD motor_3(PWM_DIR, 22, 20); // PWM 2 = Pin 9, DIR 2 = Pin 10.
 CytronMD motor_4(PWM_DIR, 26, 21); // PWM 2 = Pin 9, DIR 2 = Pin 10.
-
-
 #endif
-
-#ifdef _Motor_cytron_md30_
-
-Motor_cytron_md30c motor_1(6, 7);
-Motor_cytron_md30c motor_2(8, 9);
-Motor_cytron_md30c motor_3(12, 14);
-
-#endif
-
-RPI_PICO_Timer ITimer(0);
-
-bool TimerHandler_Calculate_RPM(struct repeating_timer *t)
-{
-  (void)t; // This line is used to suppress unused parameter warnings
-  RPM = ((counts / 4 - previous_encoder_ticks) * 60 * 1000) / PPR / RPM_TIMER_INTERVAL_MS;
-  previous_encoder_ticks = counts / 4;
-  return true; // Run Countinously
-}
 
 void run_commands(char COMMAND)
 {
@@ -109,35 +71,15 @@ void run_commands(char COMMAND)
 
   case READ_ENCODERS:
   {
-    Serial.print(counts);
-    Serial.print(" ");
-    Serial.print(counts);
-    Serial.print(" ");
-    Serial.println(counts);
+    // Serial.print(" ");
+    // Serial.print(counts);
+    // Serial.print(counts);
+    // Serial.print(" ");
+    // Serial.println(counts);
     break;
   }
   }
 }
-
-// void readEncoder()
-// {
-//   static int8_t prevA = 0;
-//   static int8_t prevB = 0;
-
-//   // int8_t currA = digitalRead(ENCODER_1_IN1);
-//   // int8_t currB = digitalRead(ENCODER_1_IN2);
-
-//   // Determine the direction of rotation
-//   int8_t direction = (prevA ^ currB) - (prevB ^ currA);
-
-//   if (direction == 1 || direction == -1)
-//   {
-//     counts += direction;
-//   }
-
-//   prevA = currA;
-//   prevB = currB;
-// }
 
 void setup()
 {
@@ -151,13 +93,12 @@ void setup()
   motor_3.setSpeed(0);
   motor_4.setSpeed(0);
 
-  delay(2000);
-  
-  // pinMode(ENCODER_1_IN1, INPUT_PULLUP); // Use internal pull-up resistors
-  // pinMode(ENCODER_1_IN2, INPUT_PULLUP);
+  delay(1000);
 
-  // attachInterrupt(digitalPinToInterrupt(ENCODER_1_IN1), readEncoder, CHANGE);
-  // attachInterrupt(digitalPinToInterrupt(ENCODER_1_IN2), readEncoder, CHANGE);
+  // Status led init
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  
 }
 
 // CORE 1 Main Loop
@@ -187,32 +128,5 @@ void loop()
   Serial.println(DriveData.motor_1);
   Serial.println(DriveData.motor_2);
   Serial.println(DriveData.motor_3);
-#endif
-}
-
-// RP2040 Second core
-void setup1()
-{
-  if (ITimer.attachInterruptInterval(RPM_TIMER_INTERVAL_MS * 1000, TimerHandler_Calculate_RPM))
-    ;
-  // Serial.println(millis());
-  else
-    Serial.println(F("Can't set Timer Callback. Select another Timer, freq. or timer"));
-}
-
-void loop1()
-{
-  // Serial.println(counts / 4);
-  // Serial.println(RPM);
-
-#if (_LOG_PID_STATS_)
-  uint32_t time = millis();
-  Serial.print(time / 1000);
-  Serial.print(" ");
-  Serial.print(Input);
-  Serial.print("Output = ");
-  Serial.print(Output);
-  Serial.print(" ");
-  Serial.println(Setpoint);
 #endif
 }
